5b3bf8db27b0498f46d5bb33e7dd7f53a011c3f0
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2026-01-05 08:42:51 +0000

    fix: correct string quotes in test_fps.py

--- test_fps.py
+++ test_fps.py
@@ -1,12 +1,11 @@
 import time
 
 import torch
-
 from model.MVANet import MVANet
 
-data = torch.randn(1, 3, 1024, 1024, device='cuda', dtype=torch.float32)
+data = torch.randn(1, 3, 1024, 1024, device="cuda", dtype=torch.float32)
 
-model = MVANet().eval().cuda()  
+model = MVANet().eval().cuda()
 for _ in range(10):
     model(data)
 
